9fd55cdc23192e94b491d342e98c2c3cb0cca1e5
Fredrik <io.qrdr@gmail.com>
2024-03-06 19:12:17 +0100

    wip

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,20 +5,46 @@ import { useState } from "react";
 type Field = (
   add: () => void,
   remove: (index: number) => void,
-  index: number
+  index: number,
+  forwards: (id: string) => {
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  }
 ) => React.ReactNode;
 
 function ArrayField({ id, fields }: { id: string; fields: Field }) {
-  const [items, setItems] = useState<Field[]>([fields]);
+  const [items, setItems] = useState<
+    {
+      values: Record<string, string>;
+      component: Field;
+    }[]
+  >([{ values: {}, component: fields }]);
 
-  const add = () => setItems([...items, fields]);
+  const add = () => {
+    setItems([...items, { values: {}, component: fields }]);
+  };
 
   const remove = (index: number) => {
     setItems([...items.slice(0, index), ...items.slice(index + 1)]);
   };
 
   return (
-    <section>{items.map((item, index) => item(add, remove, index))}</section>
+    <section>
+      {items.map((item, index) =>
+        item.component(add, remove, index, (name: string) => ({
+          value: items[index].values[name],
+          onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+            setItems([
+              ...items,
+              // {
+              //   ...items[index],
+              //   values: { id: event.target.id, value: event.target.value },
+              // },
+            ]);
+          },
+        }))
+      )}
+    </section>
   );
 }
 
@@ -27,13 +53,19 @@ export default function Page() {
     <main className="flex mx-auto min-h-screen flex-col px-6 py-10 max-w-[800px]">
       <ArrayField
         id="item"
-        fields={(add, remove, index) => (
+        fields={(add, remove, index, forwards) => (
           <article key={index} className="flex gap-4">
-            <input type="text" className="border border-zinc-900" />
+            <input
+              {...forwards}
+              type="text"
+              className="border border-zinc-900"
+            />
 
             <button onClick={() => remove(index)}>remove</button>
             <button onClick={() => add()}>add</button>
             <p>{index}</p>
+
+            <input type="radio" id="huey" name="drone" {...forwards} />
           </article>
         )}
       />
