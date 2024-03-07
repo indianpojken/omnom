e20afdca97fe93a52adca898d3afec18d6ae6307
Fredrik <io.qrdr@gmail.com>
2024-03-07 15:31:17 +0100

    wip: further tests

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,44 +2,52 @@
 
 import { useState } from "react";
 
-type Field = (
+type Field<T> = (
   add: () => void,
   remove: (index: number) => void,
   index: number,
-  forwards: (id: string) => {
-    value: string;
+  forwards: (name: keyof T) => {
+    value: T[keyof T];
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   }
 ) => React.ReactNode;
 
-function ArrayField({ id, fields }: { id: string; fields: Field }) {
-  const [items, setItems] = useState<
-    {
-      values: Record<string, string>;
-      component: Field;
-    }[]
-  >([{ values: {}, component: fields }]);
+type FieldState<T> = {
+  values: T;
+  component: Field<T>;
+}[];
+
+function ArrayField<T>({ id, fields }: { id: string; fields: Field<T> }) {
+  const [items, setItems] = useState<FieldState<T>>([
+    { values: {} as T, component: fields },
+  ]);
 
   const add = () => {
-    setItems([...items, { values: {}, component: fields }]);
+    setItems([...items, { values: {} as T, component: fields }]);
   };
 
   const remove = (index: number) => {
     setItems([...items.slice(0, index), ...items.slice(index + 1)]);
   };
 
+  console.log(items.map((item) => item.values));
+
   return (
     <section>
       {items.map((item, index) =>
-        item.component(add, remove, index, (name: string) => ({
+        item.component(add, remove, index, (name) => ({
           value: items[index].values[name],
           onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-            setItems([
-              ...items,
-              // {
-              //   ...items[index],
-              //   values: { id: event.target.id, value: event.target.value },
-              // },
+            setItems((prevItems) => [
+              ...prevItems.slice(0, index),
+              {
+                ...prevItems[index],
+                values: {
+                  ...prevItems[index].values,
+                  [name]: event.target.value,
+                },
+              },
+              ...prevItems.slice(index + 1),
             ]);
           },
         }))
@@ -51,12 +59,12 @@ function ArrayField({ id, fields }: { id: string; fields: Field }) {
 export default function Page() {
   return (
     <main className="flex mx-auto min-h-screen flex-col px-6 py-10 max-w-[800px]">
-      <ArrayField
+      <ArrayField<{ food: string; allergen: number }>
         id="item"
         fields={(add, remove, index, forwards) => (
           <article key={index} className="flex gap-4">
             <input
-              {...forwards}
+              {...forwards("food")}
               type="text"
               className="border border-zinc-900"
             />
@@ -65,7 +73,19 @@ export default function Page() {
             <button onClick={() => add()}>add</button>
             <p>{index}</p>
 
-            <input type="radio" id="huey" name="drone" {...forwards} />
+            <input
+              type="radio"
+              id="huey"
+              name="drone"
+              {...forwards("allergen")}
+            />
+
+            <input
+              type="radio"
+              id="huey"
+              name="drone"
+              {...forwards("allergen")}
+            />
           </article>
         )}
       />
