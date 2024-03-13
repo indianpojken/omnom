485f0cd3125b51799e9ef213394d4755c1a72e73
Fredrik <io.qrdr@gmail.com>
2024-03-13 12:50:33 +0100

    fix: remove unused imports

--- src/components/Forms/editRestaurant/index.tsx
+++ src/components/Forms/editRestaurant/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { useState } from "react";
-import { motion } from "framer-motion";
 
 import { municipalities } from "@/constants";
 import Field from "@/components/Form/Field";
