9e0cab4d88c04d985a4ffe04af26b7d05ec621a1
Fredrik <io.qrdr@gmail.com>
2024-03-13 12:01:35 +0100

    feat: add animations to municipal page

--- src/app/(pages)/municipal/[municipal]/layout.tsx
+++ src/app/(pages)/municipal/[municipal]/layout.tsx
@@ -2,12 +2,12 @@ import Logo from "@/components/Logo/Logo";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex mx-auto min-h-screen flex-col px-6 py-10 max-w-[800px]">
+    <>
       <header>
         <Logo />
       </header>
 
       {children}
-    </main>
+    </>
   );
 }
