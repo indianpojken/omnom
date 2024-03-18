99b327079904124561963a64f5017acd9392f5f8
Fredrik <io.qrdr@gmail.com>
2024-03-18 23:01:32 +0100

    fix: move icon to icons

--- src/components/UserToolbar/index.tsx
+++ src/components/UserToolbar/index.tsx
@@ -33,18 +33,7 @@ export default function UserToolbar({
     <article className="flex flex-col rounded-md text-amber-900 border-b-amber-900 border-b-2 bg-amber-100 p-2">
       <section className="flex">
         <section className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          {icons["person"]}
 
           <p className="font-bold">{user.email}</p>
         </section>
