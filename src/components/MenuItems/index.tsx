52a355d6aa65eaec1b10e2ae9c2389aaa809fbe8
Fredrik <io.qrdr@gmail.com>
2024-03-18 19:02:50 +0100

    feat: allergies on side

--- src/components/MenuItems/index.tsx
+++ src/components/MenuItems/index.tsx
@@ -5,10 +5,10 @@ export default function MenuItems({ items }: { items: Menu[""]["items"] }) {
   return (
     <section className="flex flex-col gap-4">
       {items.map((item) => (
-        <article className="flex flex-col gap-2" key={item.food}>
-          <section className="flex gap-4">
-            <article className="flex flex-1 items-center bg-amber-50 border-amber-900 rounded-md overflow-hidden border-b-2">
-              <aside className="p-2 flex text-amber-900 bg-amber-100">
+        <article className="flex gap-2" key={item.food}>
+          <section className="flex flex-1 gap-4">
+            <article className="flex flex-1  items-center bg-amber-50 border-amber-900 rounded-md overflow-hidden border-b-2">
+              <aside className="p-2 h-full flex items-center text-amber-900 bg-amber-100">
                 {icons["food"]}
               </aside>
 
@@ -19,7 +19,7 @@ export default function MenuItems({ items }: { items: Menu[""]["items"] }) {
           {(item.allergies.length || item.vegetarian) && (
             <ol className="flex gap-2 flex-wrap">
               {item.vegetarian && (
-                <p className="justify-center select-none items-center flex text-nowrap p-2 rounded-md text-xs uppercase font-semibold bg-green-300 text-green-900 p border-b-2 border-green-900">
+                <p className="flex-1 justify-center select-none items-center flex text-nowrap p-2 rounded-md text-xs uppercase font-semibold bg-green-300 text-green-900 p border-b-2 border-green-900">
                   Vegetarisk
                 </p>
               )}
@@ -27,7 +27,7 @@ export default function MenuItems({ items }: { items: Menu[""]["items"] }) {
               {item.allergies.map((allergy) => (
                 <li
                   key={allergy}
-                  className="flex-0 justify-center flex transition-colors select-none p-2 rounded-md text-xs uppercase font-semibold bg-amber-200 text-amber-900 border-b-2 border-amber-900"
+                  className="flex-1 justify-center items-center flex transition-colors select-none p-2 rounded-md text-xs uppercase font-semibold bg-amber-200 text-amber-900 border-b-2 border-amber-900"
                 >
                   {allergy}
                 </li>
