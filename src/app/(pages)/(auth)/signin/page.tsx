5947b05476bd6b8cb31e5f33ccd2975af1b70b1f
Fredrik <io.qrdr@gmail.com>
2024-03-18 20:09:27 +0100

    fix: width on sign- up and in

--- src/app/(pages)/(auth)/signin/page.tsx
+++ src/app/(pages)/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@ import SignIn from "@/components/Forms/SignIn";
 
 export default async function Page() {
   return (
-    <article className="m-auto items-center w-96">
+    <article className="m-auto items-center w-full max-w-96">
       <SignIn />
     </article>
   );
