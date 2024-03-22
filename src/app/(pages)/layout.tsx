bfc9d152c43ff26a0466048a3f0e233dbb1ecd9b
Fredrik <io.qrdr@gmail.com>
2024-03-22 13:03:13 +0100

    Revert "animate logo"
    This reverts commit be3fe30ea768ef5ab96d6fa94686e66547f0a822.

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,12 +1,9 @@
 import Header from "@/components/Header";
-import { MotionArticle } from "@/components/Motion";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <MotionArticle initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <Header />
-      </MotionArticle>
+      <Header />
 
       <main>{children}</main>
     </>
